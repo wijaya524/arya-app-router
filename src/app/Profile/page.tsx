a3a28eb0566b0259fea47a9c6fb9e484c88a272c
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-05-07 22:16:15 +0700

    first commit

---
+++ src/app/Profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Profile</h1>
+        </main>
+    );
+}
---
+++ src/app/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+export default function Navbar() {
+    return(
+        <nav className=" flex py-5 px-5 bg-slate-600 justify-between items-center">
+            <h1>Navbar</h1>
+            <ul className="flex justify-around gap-5">
+                <Link href={"/"}><li>Home</li></Link> 
+                <Link href={"/Profile"}><li>Profile</li></Link> 
+                <Link href={"/about"}><li>About</li></Link> 
+            </ul>
+        </nav>
+    )
+}
