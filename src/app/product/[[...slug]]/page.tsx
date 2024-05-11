e0682f92970510ba70188b21e52185ee76aa1269
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-05-12 05:56:44 +0700

    w

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,18 +1,6 @@
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/productAPI')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
-}
- 
+import { getData } from "@/service/product"
 export default async function Page() {
-  const products = await getData()
+  const products = await getData('http://localhost:3000/api/productAPI')
   console.log(products)
   return(
     <div>
