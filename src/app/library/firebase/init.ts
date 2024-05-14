e2e53a9059a8fc54ef28799fbfe60872e0a14f6b
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-05-14 21:26:51 +0700

    yy

--- src/app/library/firebase/init.ts
+++ src/app/library/firebase/init.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_eFdQyC1JzuAVj_aseX54mzxpG1-LXBs",
-  authDomain: "app-router-next.firebaseapp.com",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: "app-router-next",
-  storageBucket: "app-router-next.appspot.com",
-  messagingSenderId: "458549939029",
-  appId: "1:458549939029:web:8e8cffe787b63c78653ddc",
-  measurementId: "G-HXGYXE1MKG"
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
--- src/app/product/loading.tsx
+++ src/app/product/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="  grid grid-cols-4 place-items-center mt-20 gap-10 animate-pulse">
+    <div className="  flex flex-wrap items-center justify-center mt-20 gap-10 w-full max-w-full min-w-96 animate-pulse">
 
 
          <div className=" w-72 h-96 max-w-sm bg-gray-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
