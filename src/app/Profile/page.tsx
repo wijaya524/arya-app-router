ca76fa96e067e63db7761285bfd9dde45112569f
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-05-15 13:43:56 +0700

    y

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -1,4 +1,17 @@
+"use client"
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Profile() {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            router.push("/login")
+        }
+    }, [router, status])
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Profile</h1>
