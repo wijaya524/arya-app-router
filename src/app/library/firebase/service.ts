05012606abb58c8c9ba6fa3ef181224968f5849c
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-05-21 21:16:55 +0700

    hehe

--- src/app/library/firebase/service.ts
+++ src/app/library/firebase/service.ts
@@ -24,32 +24,37 @@ export async function retrieveDataById(colectionName: string, id: string) {
     return data;
 }
 
-//Register
-export async function register(data:{
-    fullname: string;
-    email: string;
-    password: string;
-    role? : string;
-}, callback: Function,) {
-    const q = query(collection(firestore, "users"), where("email", "==", data.email)
-);
-const snapshot = await  getDocs(q)
-const users = snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data()
-}))
-if(users.length > 0) {
-    callback({status: false, message: "Email already exist"})
-}else {
-    data.role = "admin";
-    data.password = await bcrypt.hash(data.password, 10);
 
-    await addDoc(collection(firestore, "users"), data)
-    .then(() => {
-        callback({status: true, message: "Register success"})
-    })
-    .catch((err) => {
-        callback({status: false, message: err})
-    })
-}
-}
+//Register
+export async function register(
+    userData: {
+      email: string;
+      password: string;
+      fullname: string;
+      role: string;
+    },
+    callback: Function
+  ) {
+    const q = query(
+      collection(firestore, "users"),
+      where("email", "==", userData.email)
+    );
+    const snapshot = await getDocs(q);
+    const data = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    if (data.length > 0) {
+      callback({ status: false, message: "Email already exists" });
+    } else {
+      userData.password = await bcrypt.hash(userData.password, 10);
+      userData.role = "member";
+      await addDoc(collection(firestore, "users"), userData)
+        .then(() => {
+          callback({ status: true, message: "Register success" });
+        })
+        .catch((error) => {
+          callback({ status: false, message: error });
+        });
+    }
+  }
