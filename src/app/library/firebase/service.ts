a5356e6531cbcee0826828f4b718e9bdfe2a2c77
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-05-23 14:47:09 +0700

    j

--- src/app/library/firebase/service.ts
+++ src/app/library/firebase/service.ts
@@ -2,7 +2,7 @@ import { collection, getDocs, getFirestore, getDoc, doc, query, where, addDoc } 
 import app from "./init";
 import bcrypt from "bcrypt"
 
-
+//Using to access fireStore on firebase
 const firestore = getFirestore(app);
 
 //Firebase service
@@ -19,14 +19,18 @@ export async function retrieveData(colectionName: string) {
 
 //Get data product id from firebase
 export async function retrieveDataById(colectionName: string, id: string) {
+    //Snapshot using to access data on database
     const snapshot = await getDoc(doc(firestore, colectionName, id));
+     //Snapshot using to access data on database
     const data = snapshot.data();
+    //Return data
     return data;
 }
 
 
 //Register
 export async function register(
+  //Types for userData
     userData: {
       email: string;
       password: string;
@@ -34,23 +38,34 @@ export async function register(
       role: string;
     }
   ) {
+     //Query
     const q = query(
+     //FireStore and collection Name
       collection(firestore, "users"),
+     //Where using to filtering document on collection 
       where("email", "==", userData.email)
     );
+      //Snapshot using to access data on database
     const snapshot = await getDocs(q);
+      //For mapping data 
     const data = snapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
+
+    //If already have data, will show status false
     if (data.length > 0) {
-       return{ status: false, statusCode: 400, message: "Email already exists" };
-    } else {
+      return{ status: false, statusCode: 400, message: "Email already exists" };
+    } 
+    //If don't have data, will show status true and hashing password to bcrypt 
+    else {
       userData.password = await bcrypt.hash(userData.password, 10);
       userData.role = "member";
       try {
+        //Add document or data to firebase 
         await addDoc(collection(firestore, "users"), userData)
         return{ status: true, statusCode: 200, message: "Register success" };
+        //If happen error
       } catch (error) {
         return{ status: false, statusCode: 500, message: "Something went wrong" };
       }
@@ -60,11 +75,16 @@ export async function register(
 
 //Take data login from firebase
 export async function login(data: {email: string}) {
+  //Query
   const q = query( 
+    //FireStore and collection Name
     collection(firestore, "users"),
+    //Where using to filtering document on collection 
     where("email", "==", data.email)
   )
+  //Snapshot using to access data on database
   const snapshot = await getDocs(q);
+  //For mapping data 
   const user = snapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
