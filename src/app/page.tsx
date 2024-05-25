08df2b03fb6b5395827f60c34543f73d402b1dbc
Aryansyah Yudha <145108713+wijaya524@users.noreply.github.com>
2024-05-26 00:36:26 +0700

    ha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Corrousel from "@/components/core/corrousel/page";
 
 
 //Home page or landing page
@@ -6,7 +7,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home</h1>
-
+      <Corrousel />
     </main>
   );
 }
